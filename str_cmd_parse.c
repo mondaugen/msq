3cbc744b370361d8896d9a8e0e11d0442f384c21
mondaugen <nicholas.esterer@gmail.com>
2017-07-07 19:54:12 -0400

    str_cmd_parse passes tests

--- str_cmd_parse.c
+++ str_cmd_parse.c
@@ -6,27 +6,31 @@
 #include "str_cmd_parse.h" 
 #include <stddef.h> 
 #include <string.h> 
+#include <stdio.h> 
 
 void
 str_cmd_parse (char *str, sgetopt_t **cmds)
 {
+    /* TODO: There should be a way to do this without the array optstarts but I
+    can't get it to work right now. */
     char *optstarts[strlen(str)+1];
-    size_t optstart_i;
+    size_t optstart_i = 0;
     optstarts[0] = str;
     optstart_i++;
     char *nextstart = NULL;
     while ((nextstart = strchr(optstarts[optstart_i-1],' '))) {
-        optstarts[optstart_i++] = nextstart;
+        optstarts[optstart_i++] = nextstart+1;
     }
-    optstarts[optstart_i] = str + strlen(str);
+    /* one past the end */
+    optstarts[optstart_i] = str + strlen(str) + 1;
     size_t i;
-    sgetopt_t *lastcmd, *done = NULL;
+    sgetopt_t *lastcmd = NULL, *done = NULL;
     char *lastarg = NULL;
     for (i = 0; i < optstart_i; i++) {
-        size_t slen = optstarts[i+1]-optstarts[i];
+        size_t slen = optstarts[i+1]-optstarts[i]-1;
         sgetopt_t **_cmd = cmds;
-        while (*_cmd) {
-            if (strncmp(optstarts[i],(*_cmd)->opt,slen) == 0) {
+        while (*_cmd && slen) {
+            if ((strncmp(optstarts[i],(*_cmd)->opt,slen) == 0)) {
                 if (optstarts[i] != str) { *(optstarts[i] - 1) = '\0'; }
                 if (lastcmd) {
                     lastcmd->fun((lastarg == optstarts[i]) ? NULL : lastarg,
--- test/str_cmd_parse_test.c
+++ test/str_cmd_parse_test.c
@@ -4,6 +4,7 @@
 
 static int correct = 1;
 static int cmp_str_idx = 0;
+static int incorrect_idx;
 static char *correct_strs[] = {
     "123",
     "789-hi bob",
@@ -11,20 +12,24 @@ static char *correct_strs[] = {
     NULL,
     NULL,
     NULL,
-    "123",
+    "-hoa",
     "789-hi",
-    ""
+    "",
+    "bob",
+    "bobby "
 };
 
 void fun (char *arg, void *aux)
 {
     char *_aux = aux;
-    printf("%s: %s\n",_aux,arg);
+    printf("%s: %s|\n",_aux,arg);
     if (arg && correct_strs[cmp_str_idx]) {
         correct &= (strcmp(correct_strs[cmp_str_idx],arg) == 0);
     } else {
         correct &= (arg == correct_strs[cmp_str_idx]);
     }
+    if (!correct) { incorrect_idx = cmp_str_idx; }
+    cmp_str_idx++;
 }
 
 sgetopt_t cmds[] = {
@@ -36,11 +41,20 @@ sgetopt_t cmds[] = {
 
 int main (void) {
     char str1[] = " -hi 123 -ho 789-hi bob -hi -ho";
-    char str2[] = " -hi -ho -hi 123 -ho 789-hi -hi ";
+    char str2[] = " -hi -ho -hi -hoa -ho 789-hi -hi ";
+    char str3[] = "-hi bob";
+    char str4[] = "-ho bobby ";
+    char str5[] = "";
     sgetopt_t *pcmds[] = {
         &cmds[0],&cmds[1],&cmds[2],&cmds[3],NULL
     };
     str_cmd_parse(str1,pcmds);
+    str_cmd_parse(str2,pcmds);
+    str_cmd_parse(str3,pcmds);
+    str_cmd_parse(str4,pcmds);
+    str_cmd_parse(str5,pcmds);
+    if (correct) { printf("correct\n"); }
+    else { printf("incorrect starting: %d\n",incorrect_idx); }
     return correct == 1 ? 0 : -1;
 }
 
